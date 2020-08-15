428222ed326f943db6637e7637efe1d62221e42a
Tomas Votruba <tomas.vot@gmail.com>
2020-08-15 13:06:51 +0200

    [CI] Add Rector CI (#2060)
    * [ci] add rector-ci
    
    * test ci
    
    * update dependencies
    
    * add easy-ci
    
    * use PHP 7.4 for Rector CI
    
    * use mixed
    
    * hotfix
    
    * [rector] hotfix
    
    Co-authored-by: rector-bot <tomas@getrector.org>

---
+++ src/Console/Option/OptionName.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\SetConfigResolver\Console\Option;
+
+final class OptionName
+{
+    /**
+     * @var string[]
+     */
+    public const CONFIG = ['--config', '-c'];
+
+    /**
+     * @var string[]
+     */
+    public const SET = ['--set', '-s'];
+}
