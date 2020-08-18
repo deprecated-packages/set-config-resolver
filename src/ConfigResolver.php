a3848e584fcb30f688c5eede688ed994ebd4d014
TomasVotruba <tomas.vot@gmail.com>
2020-08-18 17:45:03 +0200

    open 8.3-dev

---
+++ src/ConfigResolver.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\SetConfigResolver;
+
+/**
+ * @see \Symplify\SetConfigResolver\Tests\ConfigResolver\ConfigResolverTest
+ */
+final class ConfigResolver extends AbstractConfigResolver
+{
+}
